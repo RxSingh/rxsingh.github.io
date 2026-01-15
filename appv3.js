4a4d0ca5653b9b6140b4110b4950b4495d0e70d6
Ranveer <47960764+RxSingh@users.noreply.github.com>
2026-01-15 01:17:37 -0500

    Delete receivedData after logging
    Remove receivedData object after logging its properties.

--- appv3.js
+++ appv3.js
@@ -4,3 +4,4 @@ const receivedData = window.myAppData;
 console.log('Data from bookmarklet:', receivedData.pageTitle);
 console.log('Data from bookmarklet:', receivedData.user);
 console.log('Data from bookmarklet:', receivedData.timestamp);
+delete receivedData
