6cd05702eadb533c3a3b511e789a4f43be6d75a5
Ranveer <47960764+RxSingh@users.noreply.github.com>
2026-01-23 22:52:37 -0500

    Simplify assignment of receivedData from window object
    Removed conditional check for receivedData and simplified its assignment.

--- appv3.js
+++ appv3.js
@@ -1,9 +1,10 @@
 alert("V_1.0 Check the console log for variables values passed thru the bookmark");
 // Access the data directly from the window object
-if (typeof receivedData == "undefined")
-    {let receivedData = window.myAppData;}
-else
-    {receivedData = window.myAppData;}
+let receivedData = window.myAppData;
+// if (typeof receivedData == "undefined")
+//    {let receivedData = window.myAppData;}
+// else
+//    {receivedData = window.myAppData;}
 console.log('Data from bookmarklet:', receivedData.pageTitle);
 console.log('Data from bookmarklet:', receivedData.user);
 console.log('Data from bookmarklet:', receivedData.timestamp);
