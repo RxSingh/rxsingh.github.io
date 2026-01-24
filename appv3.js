9d7eb8f12f5895a2f6d04292d2ba715195370210
Ranveer <47960764+RxSingh@users.noreply.github.com>
2026-01-23 23:40:08 -0500

    Update alert messages and clean up receivedData

--- appv3.js
+++ appv3.js
@@ -1,13 +1,5 @@
-"use strict";
-alert("V_2.1 Check the console log for variables values passed thru the bookmark");
-// Access the data directly from the window object
+alert("V_2.2 Check the console log for variables values passed thru the bookmark");
 let receivedData = window.myAppData;
-// if (typeof receivedData == "undefined"){
-//    alert("receivedData is undefined... defining and assigning value to it");
-//    let receivedData = window.myAppData;}
-// else{
-//    alert("receivedData is already defined... assigning value to it");
-//    receivedData = window.myAppData;}
 console.log('Data from bookmarklet:', receivedData.pageTitle);
 console.log('Data from bookmarklet:', receivedData.user);
 console.log('Data from bookmarklet:', receivedData.timestamp);
@@ -16,6 +8,4 @@ delete receivedData.pageTitle;
 delete receivedData.user;
 delete receivedData.timestamp;
 delete receivedData
-receivedData = null;
-receivedData = undefined;
 alert("Cleared variables data for next run");
